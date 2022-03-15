58ba8845b5e68533bf8d3fbc7299b8e00cb880f9
HiteshWork13 <hiteshwork13@gmail.com>
2022-03-15 10:12:43 +0530

    feat : project set up

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IconsProviderModule } from "./icons-provider.module";
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    IconsProviderModule
+  ]
+})
+export class SharedModule { }
