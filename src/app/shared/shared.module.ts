fb099e78b70df8ea6b63ffe43928ad1a3bfee770
Hitesh <hiteshwork13@gmail.com>
2022-03-29 08:39:53 +0530

    Fix : minor updates

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 // import { IconsProviderModule } from './icons-provider.module';
 import { NgZorroAntdModule } from './ng-zorro-antd.module';
-import { TableWidgetComponentsModule } from './table//table-widget-components/table-widget-components.module';
+import { TableWidgetComponentsModule } from './table/table-widget-components/table-widget-components.module';
 import { TableComponent } from './table/table.component';
 
 @NgModule({
