515483f73a5fe9a7b7653809b3fb6ec4fc291242
Hitesh <mailto:hiteshwork07@gmail.com>
2022-06-09 18:27:03 +0530

    fix : add total cost field validator

--- src/app/shared/components/packages/packages.component.ts
+++ src/app/shared/components/packages/packages.component.ts
@@ -227,4 +227,13 @@ export class PackagesComponent implements OnInit {
   onSubmit() {
     //
   }
+
+  onlyNumber(evt) {
+    evt = evt ? evt : window.event;
+    var charCode = evt.which ? evt.which : evt.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+      return false;
+    }
+    return true;
+  }
 }
